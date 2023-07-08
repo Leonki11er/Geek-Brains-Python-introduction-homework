efaba9bf4667ba53850afe8acd6f333b212a191f
Leonid_Levenko <leonidustal@gmail.com>
2023-07-08 11:13:38 +0700

    Task30 completed

--- Lesson6/Task30.py
+++ Lesson6/Task30.py
@@ -1,8 +1,7 @@
-first_element=int(input('Введите первый элемент: '))
-difference=int(input('Введите разность: '))
-n=int(input('Введите количество элементов: '))
+first_element = int(input('Введите первый элемент: '))
+difference = int(input('Введите разность: '))
+n = int(input('Введите количество элементов: '))
 number_list = []
 for i in range(n):
     number_list.append(first_element + i * difference)
 print(number_list)    
-
